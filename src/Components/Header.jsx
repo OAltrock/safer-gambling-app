2370410e50ebecd8eb2cd71828af8412550483a9
Oliver Altrock <oliver.altrock@fdmgroup.com>
2024-06-28 12:40:59 +0200

    added a help page. added a link to the hompage when clicking the fdm icon.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,12 @@
 import logo from "../Assets/fdm_logo.png";
+import { useNavigate } from "react-router-dom";
 import "../Styles/Header.css";
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <div className="header">
-      <img className="logo" alt=" logo" src={logo} width={150} height={75} />
+      <img className="logo" alt=" logo" onClick={() => navigate("/")} src={logo} width={150} height={75} />
     </div>
   );
 };
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 import "../Styles/Navbar.css";
 import HelpModal from './HelpModal';
 import SettingsModal from './SettingsModal';
 
-const Navbar = () => {
+
+function Navbar() {
+  const navigate = useNavigate();
+
   const [showHelpPopup, setShowHelpPopup] = useState(false);
   const [showSettingsPopup, setShowSettingsPopup] = useState(false);
 
@@ -15,10 +19,14 @@ const Navbar = () => {
     setShowSettingsPopup(true);
   };
 
+  const handleGuidanceClick = () => {
+    navigate("/Guidance");
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-buttons">
-        <button className="navbar-button">Advice & Guidance</button>
+        <button className="navbar-button" onClick={handleGuidanceClick} >Advice & Guidance</button>
         <button className="navbar-button" onClick={handleHelpClick}>Help</button>
         <button className="navbar-button" onClick={handleSettingsClick}>Settings</button>
       </div>
