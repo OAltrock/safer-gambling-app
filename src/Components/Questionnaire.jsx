8a6987633232ddf5f4a11845cab6d40fc6f9e1ec
Archana Patil <archana.patil@fdmgroup.com>
2024-09-10 13:18:29 +0100

    pop-up texts added as tooltip

--- src/Components/Questionnaire.jsx
+++ src/Components/Questionnaire.jsx
@@ -7,89 +7,86 @@ import { setQuestionnaireDoneTrue } from '../slices/questionnaireDoneSlice.js';
 import { Container } from "react-bootstrap";
 import Carousel from "react-bootstrap/Carousel";
 import Form from 'react-bootstrap/Form';
+import { Tooltip, OverlayTrigger } from 'react-bootstrap';
 
 const Questionnaire = () => {
 
   let dispatch = useDispatch();
-  let questionnaire = useSelector(state => state.questionnaire)  
-  const navigate = useNavigate();  
-  let [header, questionText, preposition, button] = useSelector(state => state.languages[state.languages.current].questionnaire.text)
-  
-  const questions = useSelector(state => state.languages[state.languages.current].questionnaire.questions);  
-  const totalQuestions = questions.length;  
-  //checks if questions has been answered (has a postive value)
+  let questionnaire = useSelector(state => state.questionnaire);
+  const navigate = useNavigate();
+  let [header, questionText, preposition, button] = useSelector(state => state.languages[state.languages.current].questionnaire.text);
+
+  const questions = useSelector(state => state.languages[state.languages.current].questionnaire.questions);
+  const totalQuestions = questions.length;
   const answeredQuestions = Object.values(questionnaire)
-    .reduce((acc, value, index) => {     
+    .reduce((acc, value, index) => {
       if (value.score >= 0) {
-        return acc += 1
+        return acc += 1;
       }
-      else return acc
+      else return acc;
     }, 0);
   const progress = (answeredQuestions / totalQuestions) * 100;
   const [index, setIndex] = useState(0);
 
-  const options = useSelector(state => state.languages[state.languages.current].questionnaire.options); 
+  const options = useSelector(state => state.languages[state.languages.current].questionnaire.options);
+  const popups = useSelector(state => state.languages[state.languages.current].questionnaire.popups);
 
-  //can be moved to where we want to evaluate the questionnaire since we can access the redux store from anywhere 
-  /* const calculateTotalScore = () => {
-    return Object.values(scores).reduce((total, score) => total + score, 0);
-  }; */
-  
-
-  /**result is being stored in redux store accessible from anywhere (useSelector(state => state.questionnaire))
-  * questionnaire is an object,eg: {question1: 1,question2: 3,...}
-  * questions can be set by dispatch {@link handleAnswer}
-  */
-  const handleSubmit = () => {    
-    dispatch(setQuestionnaireDoneTrue())
-    //console.log(questionnaire)    
+  const handleSubmit = () => {
+    dispatch(setQuestionnaireDoneTrue());
     navigate("/Home");
   };
 
-  const handleSelect = (selectedIndex, e) => {    
-    setIndex(selectedIndex)
-  }
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
 
-  /**
-   * uses redux reducers (dispatch {@link useDispatch}) to update questionnaire {@link questionnaire}
-   * @param {*} e SyntheticBaseEvent containing target, nativeEvent etc.
-   */
-  const handleAnswer = (e) => {   
+  const handleAnswer = (e) => {
     dispatch(setQuestion({
       id: index + 1,
       data: parseInt(e.target.value)
-    }))    
+    }));
   };
+
+  const renderTooltip = (props, popup) => (
+    <Tooltip id="button-tooltip" {...props}>
+      {popup}
+    </Tooltip>
+  );
 
   return (
     <Container fluid>
       <div>
         <h3>{header}</h3>
-        
       </div>
-      <Carousel style={{minHeight: '60vh'}} activeIndex={index} interval={null} onSelect={handleSelect}l keyboard={true}>
-        {questions.map(question => {
-          return (
-            <Carousel.Item style={{minHeight: "unset"}} key={`citem${questions.indexOf(question)}`}>
-              <h4 key={'questionCount' + index}>{questionText} {index + 1} {preposition} {questions.length} </h4>
-              <p key={`pquest${questions.indexOf(question)}`}>{question}</p>
-              <Form key={`radio${questions.indexOf(question)}`}>
-                {options.map((option, score) => (
-                  <Form.Check
-                    inline
-                    label={option}
-                    name={questions.indexOf(question)}
-                    type='radio'                    
-                    value={questionnaire['question' + (index + 1)].score !== -1 ? questionnaire['question' + (index + 1)].score : score}
-                    onChange={handleAnswer}                    
-                    key={`q${(score)}`}
-                  />
-                ))}
-              </Form>
-            </Carousel.Item>
-          )
-        })}
+
+      <Carousel style={{ minHeight: '60vh' }} activeIndex={index} interval={null} onSelect={handleSelect} keyboard={true}>
+        {questions.map((question, qIndex) => (
+          <Carousel.Item style={{ minHeight: "unset" }} key={`citem${qIndex}`}>
+            <h4 key={`questionCount${qIndex}`}>{questionText} {index + 1} {preposition} {questions.length}</h4>
+            <OverlayTrigger
+              placement="right"
+              delay={{ show: 250, hide: 400 }}
+              overlay={(props) => renderTooltip(props, popups[qIndex])}
+            >
+              <p key={`pquest${qIndex}`}>{question}</p>
+            </OverlayTrigger>
+            <Form key={`radio${qIndex}`}>
+              {options.map((option, score) => (
+                <Form.Check
+                  inline
+                  label={option}
+                  name={qIndex}
+                  type='radio'
+                  value={questionnaire[`question${index + 1}`].score !== -1 ? questionnaire[`question${index + 1}`].score : score}
+                  onChange={handleAnswer}
+                  key={`q${score}`}
+                />
+              ))}
+            </Form>
+          </Carousel.Item>
+        ))}
       </Carousel>
+
       <div className="progress">
         <div
           className="progress-bar progress-bar-striped progress-bar-animated"
@@ -99,9 +96,9 @@ const Questionnaire = () => {
           style={{ width: `${progress}%` }}
         ></div>
       </div>
-      <button style={{alignSelf:'center'}} onClick={handleSubmit} className="submit-button" disabled={answeredQuestions !== questions.length}>{button}</button>
+      <button style={{ alignSelf: 'center' }} onClick={handleSubmit} className="submit-button" disabled={answeredQuestions !== questions.length}>{button}</button>
     </Container>
-  )
-}
+  );
+};
 
 export default Questionnaire;
